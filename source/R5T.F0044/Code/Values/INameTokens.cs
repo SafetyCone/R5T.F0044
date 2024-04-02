5128b76336fc020be053e6cc8dfccd70ba31d6ab
davidcoats <david@getsafetycone.com>
2024-04-01 18:11:37 -0700

    Interim changes.

--- source/R5T.F0044/Code/Values/INameTokens.cs
+++ source/R5T.F0044/Code/Values/INameTokens.cs
@@ -8,10 +8,29 @@ namespace R5T.F0044
 	[ValuesMarker]
 	public partial interface INameTokens : IDraftValuesMarker
 	{
+        /// <summary>
+        /// <para><value>Client</value></para>
+        /// </summary>
         public string Client => "Client";
+
+        /// <summary>
+        /// <para><value>Construction</value></para>
+        /// </summary>
         public string Construction => "Construction";
+
+        /// <summary>
+        /// <para><value>Library</value></para>
+        /// </summary>
         public string Library => "Library";
+
+        /// <summary>
+        /// <para><value>Private</value></para>
+        /// </summary>
 		public string Private => "Private";
+
+        /// <summary>
+        /// <para><value>Server</value></para>
+        /// </summary>
         public string Server => "Server";
     }
 }
